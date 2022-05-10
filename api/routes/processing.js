dc1bd1183a5b0111889c41f2f2c96455dcfed6cd
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2022-05-10 12:23:33 +0530

    feat: maintain indentation and write to pdf doc

--- api/routes/processing.js
+++ api/routes/processing.js
@@ -1,10 +1,17 @@
 const router = require('express').Router()
 const fs = require('fs')
+const PDFDocument = require('pdfkit')
 
 router.get('/',(req,res)=>{
     
     const folder = './public/uploads/'+req.header('User-Name')
     var folderFiles =[]
+
+    // Creating PDF Document
+    const doc = new PDFDocument();
+
+    //Piping Output to PDF Document
+    doc.pipe(fs.createWriteStream(folder+'/'+'output.pdf'))
 
     fs.readdir(folder,(err,files) => {
         
@@ -17,13 +24,14 @@ router.get('/',(req,res)=>{
         }
 
         files.forEach(file => {
-            console.log(folder+"/"+file)
             folderFiles.push(folder+"/"+file)
         })
 
         folderFiles.forEach(file => {
-            processLineByLine(file)
+            processLineByLine(file, doc)
         })
+
+        doc.end()
         
         return res.json({
             "status":200,
@@ -34,15 +42,40 @@ router.get('/',(req,res)=>{
 
 })
 
-function processLineByLine(fileName)
+// Process file and adding to document
+function processLineByLine(fileName, doc)
 {
-    const data = fs.readFileSync(fileName,'UTF-8')
+    originalFileName = fileName.split('-');
+    originalFileName = originalFileName[originalFileName.length - 1]
+    doc.text("FILE: "+originalFileName)
+    doc.text("-------------------------------")
+    doc.text(" ")
+    
+    const data = fs.readFileSync(fileName,'utf-8')
 
-    const lines = data.split(/\r?\n/);
+    const lines = data.split('\r\n');
 
     lines.forEach((line)=>{
-        console.log(fileName+": "+line)
+        line = fixTabSpaces(line)
+        doc.text(text)
     })
+    doc.addPage()
+}
+
+// Keeps indentation of code
+function fixTabSpaces(line) {
+    line = line.split('\t')
+    text = ''
+    line.forEach(lineItem => {
+        if (lineItem == '')
+            text += "   "
+
+        else
+            text += lineItem
+    })
+    return text
 }
 
 module.exports = router
+
+
