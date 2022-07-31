d87aaf9722618edfef70d8e8837d62ba4ccc0aa8
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2022-07-31 12:28:47 +0530

    feat: syntax highlighting done

--- api/routes/processing.js
+++ api/routes/processing.js
@@ -3,9 +3,9 @@ const fs = require('fs')
 const PDFDocument = require('pdfkit')
 const hljs = require('highlight.js')
 const htmlCreator = require('html-creator')
-const pdf = require('html-pdf');
+const pdf = require('html-pdf')
 
-router.get('/', (req, res) => {
+router.get('/obselete', (req, res) => {
   const folder = './public/uploads/' + req.header('User-Name')
   const folderFiles = []
 
@@ -48,26 +48,99 @@ router.get('/', (req, res) => {
   })
 })
 
+// New Processing Route
+
+router.get('/', (req, res) => {
+  const folder = './public/uploads/' + req.header('User-Name')
+  const folderFiles = []
+
+  if (!fs.existsSync(folder)) {
+    return res.status(400).json({
+      status: 400,
+      message: 'User Folder Not Created Yet'
+    })
+  }
+
+  // Creating HTML Document
+  const html = new htmlCreator()
+
+  // adding required CDN Files
+  html.document.addElement({ type: 'head', content: [{ type: 'link', attributes: { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/default.min.css' } }] })
+  html.document.addElement({ type: 'body', content: [{ type: 'script', attributes: { src: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min.js' } }] })
+
+  fs.readdir(folder, (err, files) => {
+    if (err) {
+      return res.json({
+        status: 500,
+        message: 'Error in Reading Files'
+      })
+    }
+
+    files.forEach(file => {
+      folderFiles.push(folder + '/' + file)
+    })
+
+    folderFiles.forEach(file => {
+      processLineByLine(file, html)
+    })
+
+    // Rendering Final HTML
+    const htmlContent = html.renderHTML()
+
+    fs.writeFile(folder + '/index.html', htmlContent, (err) => {
+      if (err) {
+        console.error(err)
+        return res.json({
+          status: 400,
+          err: err
+        })
+      }
+
+      const htmlFile = fs.readFileSync(folder + '/index.html', 'utf8')
+
+      const options = { format: 'Letter' }
+
+      pdf.create(htmlFile, options).toFile(folder + '/output.pdf', (err, data) => {
+        if (err) {
+          console.error(err)
+          return res.json({
+            status: 400,
+            err: err
+          })
+        }
+        return res.json({
+          status: 200,
+          message: 'processing successful',
+          files: folderFiles,
+          data: data
+        })
+      })
+    })
+  })
+})
+
 // Process file and adding to document
-function processLineByLine (fileName, doc) {
+function processLineByLine (fileName, html) {
   let originalFileName = fileName.split('-')
   originalFileName = originalFileName[originalFileName.length - 1]
-  doc.text('FILE: ' + originalFileName)
-  doc.text('-------------------------------')
-  doc.text(' ')
+  // Start of a File
+  html.document.addElementToType('body', { type: 'br' })
+
+  // Put Name of File
+  html.document.addElementToType('body', { type: 'h1', content: originalFileName })
+  html.document.addElementToType('body', { type: 'hr' })
 
   const data = fs.readFileSync(fileName, 'utf-8')
-
   const lines = data.split('\r\n')
 
   lines.forEach((line) => {
-    
     // fix tab spaces
     line = fixTabSpaces(line)
-    // add file text to document
-    doc.text(line)
+
+    const htmlContent = hljs.highlightAuto(line).value
+    // File Contents
+    html.document.addElementToType('body', { type: 'p', content: htmlContent })
   })
-  doc.addPage()
 }
 
 // Keeps indentation of code
@@ -82,64 +155,64 @@ function fixTabSpaces (line) {
 
 // TODO - CLEAN UP
 // Test Route for Highlight.jS
-router.get('/highlight', (req,res) => {
-  
-  let html = hljs.highlightAuto('public static void main() { Scanner Sc = new Scanner(System.in)}').value
+router.get('/highlight', (req, res) => {
+  const html = hljs.highlightAuto('public static void main() { Scanner Sc = new Scanner(System.in)}').value
   console.log(html)
   return res.json({
-    status:200,
+    status: 200,
     val: html
   })
 })
 
 // TODO - CLEAN UP
 // Test Route for HTML Creator
-router.get('/htmlCreate',(req,res) => {
-
-  const html = new htmlCreator();
+router.get('/htmlCreate', (req, res) => {
+  const html = new htmlCreator()
 
   // adding required CDN Files
-  html.document.addElement({ type: 'head' , content: [{ type: 'link',  attributes:{ rel:'stylesheet',href: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/default.min.css' }}]})
-  html.document.addElement({ type: 'body', content: [{type:'script', attributes:{src:'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min.js'}}]})
-  
-  //File Contents
-  html.document.addElementToType('body', {type: 'p', content: '<span class="hljs-function"><span class="hljs-keyword">public</span> <span class="hljs-keyword">static</span> <span class="hljs-keyword">void</span> <span class="hljs-title">main</span>()</span> { Scanner Sc = <span class="hljs-keyword">new</span> Scanner(System.<span class="hljs-keyword">in</span>)}'})
-  
-  //After End of a File
-  html.document.addElementToType('body', {type:'br'})
-  
-  let htmlContent = html.renderHTML();
-  console.log(htmlContent);
+  html.document.addElement({ type: 'head', content: [{ type: 'link', attributes: { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/default.min.css' } }] })
+  html.document.addElement({ type: 'body', content: [{ type: 'script', attributes: { src: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min.js' } }] })
 
-  fs.writeFile("./public/uploads/"+"index.html", htmlContent, (err) =>{
+  // File Contents
+  html.document.addElementToType('body', { type: 'p', content: '<span class="hljs-function"><span class="hljs-keyword">public</span> <span class="hljs-keyword">static</span> <span class="hljs-keyword">void</span> <span class="hljs-title">main</span>()</span> { Scanner Sc = <span class="hljs-keyword">new</span> Scanner(System.<span class="hljs-keyword">in</span>)}' })
+
+  // After End of a File
+  html.document.addElementToType('body', { type: 'br' })
+
+  const htmlContent = html.renderHTML()
+  console.log(htmlContent)
+
+  fs.writeFile('./public/uploads/' + 'index.html', htmlContent, (err) => {
     if (err) {
-      console.error(err);
+      console.error(err)
       return res.json({
-        status:400,
-        err:err
+        status: 400,
+        err: err
       })
     }
     return res.json({
-      status:200,
+      status: 200,
       val: htmlContent
     })
   })
 })
 
-router.get('/htmlToPdf',(req,res) => {
-  let html = fs.readFileSync('./public/uploads/index.html', 'utf8')
-  let options = { format: 'Letter' }
-  
-  pdf.create(html,options).toFile('./public/uploads/output.pdf', (err,data) =>{
+// TODO - Clean Up
+// Test HTML to PDF
+router.get('/htmlToPdf', (req, res) => {
+  const html = fs.readFileSync('./public/uploads/index.html', 'utf8')
+  const options = { format: 'Letter' }
+
+  pdf.create(html, options).toFile('./public/uploads/output.pdf', (err, data) => {
     if (err) {
-      console.error(err);
+      console.error(err)
       return res.json({
-        status:400,
-        err:err
+        status: 400,
+        err: err
       })
     }
     return res.json({
-      status:200,
+      status: 200,
       val: data
     })
   })
---
+++ public/uploads/b7831b2d-127e-4c97-9388-f23d8d0c69df/1659250265062-323254705-1652163699178-644437088-Welcome.java
@@ -0,0 +1,9 @@
+public class Welcome
+{
+    public static void main(String args[])
+    {
+            System.out.println("Welcome to CSE1007");
+	    System.out.println("Whatever");
+	    Scanner Sc = new Scanner(System.in);
+    }
+}
